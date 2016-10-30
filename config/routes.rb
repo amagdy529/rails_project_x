df9fd75959263028719ba321a4140071985d6971
magdi <ahmed.magdi@brightcreations.com>
2016-10-30 20:42:58 +0200

    just a scaffold for testing called it highscores

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :high_scores
   get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
